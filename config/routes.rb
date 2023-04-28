2030c7d14dd93276f2e9c6e511263d118ecc387d
Miss-Cheese <nat.galligan@gmail.com>
2023-04-28 07:12:38 -0400

    Add React with react_on_rails gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   get 'stories/index'
   devise_for :users
   root to: 'stories#index'
